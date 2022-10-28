5b38e6b5d7ff549c74d45b3417fe00bbdf92338c
Robert Bamler <robert.bamler@uni-tuebingen.de>
2022-10-29 01:54:43 +0200

    Prevent building for Python 3.6
    If python3.6 happens to be installed on the CI system then we have to
    explicitly prevent building for it since PyO3 doesn't support it, thus
    breaking CI.
    
    Also fixes `get_versions.py` to reflect that the python wheel version is
    no longer specified separately in `pyproject.toml` but rather taken from
    `Cargo.toml`.

--- get_version.py
+++ get_version.py
@@ -3,8 +3,4 @@ import toml
 t = toml.load("Cargo.toml")
 crate_version = t['package']['version']
 
-t = toml.load("pyproject.toml")
-wheel_version = t['tool']['poetry']['version']
-
-assert crate_version == wheel_version
 print(crate_version)
